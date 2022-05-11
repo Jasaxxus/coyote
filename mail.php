9c54ec5fdc08ae3a3ca50f8ac43923f93505a422
Jasur Abduvaliev <stars.moon1703@gmail.com>
2022-05-12 03:00:04 +0500

    final

--- mail.php
+++ mail.php
@@ -1,9 +1,40 @@
-<?php
-$to = "coyotetestmail@mail.ru";
-$subject = "My subject";
-$txt = "Hello world!";
-$headers = "From: coyotetestmail@mail.ru" . "\r\n" .
-"CC: coyotetestmail@mail.ru";
+<?php 
 
-mail($to,$subject,$txt,$headers);
-?> 
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$name = $_POST['user_name'];
+$email = $_POST['user_email'];
+$message = $_POST['user_message'];
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'coyotetestmail@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'aAIr(r9POso1'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom('coyotetestmail@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('andrey.gavriilov@inbox.ru');     // Кому будет уходить письмо 
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Заявка с сайта COYOTE';
+$mail->Body    = '' .$name . ' оставил заявку <br> Его e-mail: ' .$email. '<br>Сообщение этого пользователя: ' .$message;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'Error';
+} else {
+    header('location: thank-you.html');
+}
+?>
